b2f6d5bfa1b8d78a3c9748a4bf9e59143d963840
Jia Li <55516461+michaelliftw@users.noreply.github.com>
2022-04-04 20:56:27 -0700

    create userInfo and add comment to Collab.js

--- csc648app/src/components/to-do-list/collab/Collab.js
+++ csc648app/src/components/to-do-list/collab/Collab.js
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import addIcon from "./addusericon.png";
 import "./collab.css";
+import UserInfo from "../userInfo/UserInfo";
 
 /*to define app element on our Modal import*/
 Modal.setAppElement("#root");
 
 function Collab() {
+  function handleChange(event) {
+    console.log(event.target.value);
+    setSearchEmail(event.target.value);
+  }
+  function handleAdd(event) {
+    console.log("Submitted", searchEmail);
+  }
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [searchEmail, setSearchEmail] = useState("");
   return (
     <div>
-      {/* once collab button is click, a modal will be rendered on the entire webpage */}
+      {/* Collab Button */}
       <button onClick={() => setModalIsOpen(true)} className="collab">
         <img src={addIcon} className="addicon" alt="addicon" />
       </button>
+      {/* Beginning of Modal */}
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
@@ -22,17 +32,29 @@ function Collab() {
           content: {
             width: "400px",
             height: "200px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginTop: "auto",
-            marginBottom: "auto",
+            margin: "auto",
           },
         }}
       >
         <h2>Share with people and groups</h2>
+        {/* email search bar */}
         <div>
-          <input type="text" placeholder="Add people and groups" alt="search" />
+          <input
+            type="text"
+            placeholder="Add people and groups"
+            alt="search"
+            onChange={handleChange}
+          />
+          <button onClick={handleAdd}>Search</button>
         </div>
+        <div>
+          <p>{searchEmail}</p>
+        </div>
+        {/* User Profile */}
+        <div>
+          <UserInfo />
+        </div>
+        {/*exit modal button*/}
         <div>
           <button onClick={() => setModalIsOpen(false)}>Done</button>
         </div>
