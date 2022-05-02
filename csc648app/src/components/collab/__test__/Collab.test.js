71190870b5cbb05277309988abe3e6d1f6f4bef6
Jia Li <55516461+michaelliftw@users.noreply.github.com>
2022-05-01 19:22:57 -0700

    edit testing

---
+++ csc648app/src/components/collab/__test__/Collab.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Collab from "../Collab";
+import { render, screen } from "@testing-library/react";
+
+/*it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Collab></Collab>, div);
+});
+*/
+/*
+it("renders correct names", () => {
+  const { getByTestId } = render(<Collab></Collab>);
+  expect(getByTestId("user-info-name")).toHaveTextContent("jia");
+});
+*/
