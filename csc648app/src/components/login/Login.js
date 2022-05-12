59dd74559360becb2394fa1ad81184fb69de7ed5
Jia Li <55516461+michaelliftw@users.noreply.github.com>
2022-05-11 17:09:38 -0700

    update css for login and signup page

--- csc648app/src/components/login/Login.js
+++ csc648app/src/components/login/Login.js
@@ -1,43 +1,62 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { validateLogin } from "../../redux/actions/loginActions";
-import './Login.css';
+import "./Login.css";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        
+    console.log("DISPATCH: " + username);
+    console.log("DISPATCH: " + password);
 
-        console.log("DISPATCH: " + username)
-        console.log("DISPATCH: " + password)
+    dispatch(validateLogin(username, password));
+    setUsername("");
+    setPassword("");
+  };
+  return (
+    <div className="container">
+      <h1 className="login-title"> Login</h1>
 
-        dispatch(validateLogin(username, password));
-        setUsername("");
-        setPassword("");
-    }
-    return (
-        <div className="container">
-            <h1> Login</h1>
-
-            <form className="login-form" onSubmit={(e) => handleSubmit(e)}>
-                <label className="login-label"> Username</label>
-                <input className="login-input" type="text" name="id" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <label className="login-label" htmlFor="password">Password</label>
-                <input className="login-input" type="password" name="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-
-                <button className="login-btn" type="submit">
-                    Login
-                </button>
-            </form>
-
+      <form className="login-form" onSubmit={(e) => handleSubmit(e)}>
+        <label className="login-label"> Email</label>
+        <input
+          className="login-input"
+          type="text"
+          name="id"
+          placeholder=""
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label className="login-label" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="login-input"
+          type="password"
+          name="password"
+          id="password"
+          placeholder=""
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="login-btn-div">
+          <button className="login-btn" type="submit">
+            Login
+          </button>
         </div>
-    )
-}
+        <div className="redirect-signup">
+          <h6 className="redirect1">Don't have an account? </h6>
+          <h6 className="redirect2">Sign up</h6>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
